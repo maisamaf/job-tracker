5eddf8f4400adf3a6debcb03058027dd1c3238b2
Maisam Afshar <maisam.afshar@gmail.com>
2026-06-04 16:31:27 +0200

    Remove dotenv configuration and clean up error handling in database initialization

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,13 +1,10 @@
+import "server-only";
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import * as schema from "./schema";
 
-import { config } from "dotenv";
-
-config({ path: ".env" }); // or .env.local
-
 if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is not set")
+  throw new Error("DATABASE_URL is not set");
 }
 
 const sql = neon(process.env.DATABASE_URL);
